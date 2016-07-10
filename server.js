3d7ca12865ad897300d33b96a7c524dd03f68490
Jesse Seligman <jesseseligman@gmail.com>
2016-07-10 10:43:28 -0700

    Set up express server

---
+++ server.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const express = require('express');
+const port = process.env.PORT || 8000;
+const path = require('path');
+
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const playlists = require('./routes/playlists');
+const tracks = require('./routes/tracks');
+const playlists_tracks = require('./routes/playlists_tracks');
+
+const app = express();
+
+app.disable('x-powered-by');
+
+app.use(morgan('short'));
+app.use(bodyParser.json())
+
+app.use(express.static(app.join(__dirname, 'public')));
+
+app.use(playlists);
+app.use(tracks);
+app.use(playlists_tracks);
+
+app.use((_req, res) => {
+  res.sendStatus(404);
+});
+
+// eslint-disable-next-line max-params
+app.use((err, _req, res, _next) => {
+  // eslint-disable-next-line no-console
+  if (err.status) {
+    return res.status(err.status).send(err);
+  }
+  console.error(err);
+  res.sendStatus(500);
+});
+
+app.listen(port, () => {
+  console.log('Listening on port', port);
+});
+
+module.exports = app;
