dd4894aeccdd809c1bbb9a9428a0ca546dfaa35d
Jesse Seligman <jesseseligman@gmail.com>
2016-07-11 14:01:20 -0700

    Add error handler for bad requests

--- routes/spotify_query.js
+++ routes/spotify_query.js
@@ -8,33 +8,46 @@ const request = require('request-promise');
 // const validations = require('../validations/playlists_tracks');
 const knex = require('../knex');
 
+const refineSearch = function(response, letter) {
+  const items = response.tracks.items;
+
+  const tracks = items.map((item) => {
+    return {
+          name: item.name,
+          artist: item.artists[0].name,
+          preview_url: item.preview_url
+          };
+  });
+
+  const filtered = tracks.filter((title) => {
+    return title.name.startsWith(letter);
+  });
+
+  return filtered;
+}
+
 router.get('/spotify', (req, res, next) => {
   const { letter, genre } = req.query;
 
+    request({uri: `https://api.spotify.com/v1/search?q=${letter}*%20genre:%22${genre}%22&type=track&limit=50`,
+    json: true})
+      .then((response) => {
 
-  request({uri: `https://api.spotify.com/v1/search?q=${letter}*%20genre:%22${genre}%22&type=track&limit=50`,
-  json: true})
-    .then((response) => {
-      const items = response.tracks.items;
+        const refined = refineSearch(response, letter);
 
-      const tracks = items.map((item) => {
-	      return {
-			        name: item.name,
-			        artist: item.artists[0].name,
-			        preview_url: item.preview_url
-		          };
-	    });
+        if (refined.length !== 0) {
+          return res.send(refined);
+        }
 
-      const filtered = tracks.filter((title) => {
-	      return title.name.startsWith(`${letter}`);
-      });
-      
+        const err = new Error('No tracks with this genre and letter.');
+        err.status = 400;
 
-      res.send(filtered);
-    })
-    .catch((err) => {
-      next(err);
-    })
+        throw err;
+      })
+      .catch((err) => {
+        next(err);
+      })
+
 });
 
 module.exports = router;
