43e600dcbdf0574a6ef92f554d39d9f02d3c6f1a
Jesse Seligman <jesseseligman@gmail.com>
2016-07-12 09:29:13 -0700

    Start ajax requests

--- public/js/search.js
+++ public/js/search.js
@@ -11,8 +11,62 @@ var genres = {
   "Riot Grrrl": 7
 };
 
-var genre = WINDOW.genre;
+var $save = $('#save-button');
 
+//$.ajax GET request to '/spotify' when enter is pressed
+var $searchInput = $('.search-input-field');
+
+$searchInput.keypress(input, function(event) {
+  var key = event.which;
+
+  if (key === 13) {
+    // Need to validate string
+
+    var playlistName = $searchInput.text();
+
+    var i = 0;
+
+    const getSpotify = function() {
+      if (i === playlistName.length) {
+        return;
+      }
+      var $xhr = $.ajax({
+        method: 'GET',
+        url: '/spotify',
+        dataType: 'json',
+        contentType: 'application/json',
+        data: {
+          letter: playlistName[i],
+          genre: WINDOW.genre;
+        }
+
+        $xhr.done(function(data) {
+          if ($xhr.status !== 200) {
+            return console.log('non 200 status');
+          }
+          // Append songs with artist, track name, and url
+
+          i += 1;
+        })
+
+        $xhr.fail(function(err) {
+          console.log(err);
+        })
+
+
+    });
+  }
+});
+
+$save.on('click', function(event) {
+  var $xhr = $.ajax({
+    method: 'POST',
+    url: '/playlists',
+    dataType: 'json',
+    contentType: 'application/json'
+    data: ``//data goes here!
+  });
+});
 //Start with just SAVE PLAYLIST BUTTON
 
 //On change event check to make sure character is a letter
