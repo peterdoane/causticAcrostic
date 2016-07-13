bb723cf1a03a2de6c70d5367f47f0e8425ad6b2e
Peter Doane <peterdoane@gmail.com>
2016-07-13 14:43:02 -0700

    made modal on collection and blur on search

--- public/js/search.js
+++ public/js/search.js
@@ -10,10 +10,8 @@ var genres = {
   "Hardcore Punk": 6,
   "Riot Grrrl": 7
 };
-var playlistName;
-var genre = localStorage.getItem('genre');
 
-var spaces = 0;
+Window.genre = 'Grindcore';
 
 var $save;
 
@@ -23,9 +21,14 @@ var $playlistContainer = $('#playlist-container');
 
 var playlistData = {tracks: []};
 
-const getSpotify = function(index, playlist) {
-  if (index === playlist.length) {
+var  searchInProgress  = false;
 
+
+
+const getSpotify = function(index, playlistName) {
+
+
+  if (index === playlistName.length) {
     var player = new Audio();
 
     $('.acrostic-play').click(function(event) {
@@ -38,18 +41,14 @@ const getSpotify = function(index, playlist) {
       player.play();
 
     })
-    var $buttonContainer = $('#both_buttons');
 
+    var $buttonContainer = $('#both_buttons');
     event.preventDefault();
 
-    if ($('#collection-button').length == 0) {
-      $buttonContainer.append('<a id="collection-button"class="waves-effect grey waves-light btn" href="collections.html">View Collection</a>',
-      '<a id="save-button" class="waves-effect grey waves-light btn" >Save Playlist</a>');
+    if ($('#collection-button').length == 0){
+      $buttonContainer.append('<a id="collection-button" class="waves-effect grey waves-light btn" href="collections.html">View Collection</a><a id="save-button" class="waves-effect grey waves-light btn">Save Playlist</a><a id="newSearch" class="waves-effect grey waves-light btn">Make New</a>');
     }
-
-    $save = $('#save-button');
-    activateSave();
-
+    searchInProgress  = false;
     return;
   }
 
@@ -59,40 +58,65 @@ const getSpotify = function(index, playlist) {
     dataType: 'json',
     contentType: 'application/json',
     data: {
-      letter: playlist[index],
-      genre: genre
+      letter: playlistName[index],
+      genre: 'grindcore' //Window.genre
     }
     });
 
     $xhr.done(function(track) {
+
+
       if ($xhr.status !== 200) {
+        searchInProgress = false;
+        console.log("search complete");
         return console.log('non 200 status');
       }
 
       // Append songs with artist, track name, and url
       var $player = $('<div><span id="dynamic-search"><i data-song="' + track.preview_url + '" class="fa fa-play-circle-o fa-2x acrostic-play" aria-hidden="true"></i>' + track.name + '<span id="searchartist"> by ' +  track.artist + '</span></span></div>');
 
-      if (playlistName[index + spaces] === ' ') {
-        $playlistContainer.append($('<div id="dynamic-search" class="invisible">invisible</div>'))
-
-        spaces += 1;
-      }
-
       playlistData.tracks.push(track);
 
       $playlistContainer.append($player);
 
-
-      getSpotify(index + 1, playlist);
+      getSpotify(index + 1, playlistName);
     });
 
     $xhr.fail(function(err) {
+      searchInProgress = false;
+      console.log("search error");
       console.log(err);
     });
 
 };
 
 
+$searchInput.keypress(function(event) {
+  var key = event.which;
+  if (key !== 13) {
+    return;
+  }
+  event.preventDefault();
+  $save = $('#save-button');
+  activateSave();
+
+    // Need to validate string
+
+  if(searchInProgress == false){
+
+    console.log("search function called");
+    searchInProgress = true;
+
+    inActivePage();
+
+    $playlistContainer.html('');
+    var playlistName = $searchInput.val().toUpperCase();
+    getSpotify(0, playlistName);
+    playlistData.name = playlistName;
+    playlistData.genre_id = genres[Window.genre];
+  }
+
+});
 
 var activateSave = function() {
   $save.on('click', function(event) {
@@ -114,24 +138,60 @@ var activateSave = function() {
   });
 };
 
+var getQueryVariable = function(variable) {
 
-$searchInput.keypress(function(event) {
-  var key = event.which;
-  if (key !== 13) {
-    return;
-  }
-  event.preventDefault();
-  $playlistContainer.empty();
+    //window.location.search would be something like this ?s=abcd
+    //remove '?' from the parameter
+    //window.location.search contains first character as '?'
+    var query = window.location.search.substring(1);
+    var vars = query.split('&'); //split
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
 
-    // Need to validate string
-  playlistName = $searchInput.val().toUpperCase();
+        //decodeURIComponent is a standard js method which decodes a URI component
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+    console.log('Query variable %s not found', variable);
+}
 
-  var withoutSpaces = playlistName.split(' ').join('');
-  getSpotify(0, withoutSpaces);
+//it will add a blur effect to the page and also disable the input bar
+var inActivePage = function() {
+  $('.search-input-field').addClass('blurredElement');
+  $('.search-input-field').prop('disabled', true);
 
-  playlistData.name = playlistName;
-  playlistData.genre_id = genres[genre];
+};
+
+//it will remove the blur effect and enable the input bar
+var activePage = function() {
+  $('.search-input-field').removeClass('blurredElement');
+  $('.search-input-field').prop('disabled', false);
+
+};
+
+
+$(document).on('click', "#newSearch", function () {
+  activePage();
 });
 
+
+//get the search params from the url
+var search = getQueryVariable('s');
+if (search){
+  var playlistNameGlobal = search.toUpperCase();
+  getSpotify(0, playlistNameGlobal);
+}
+
+
+//Start with just SAVE PLAYLIST BUTTON
+
+//On change event check to make sure character is a letter
+//Then make $.ajax get request to '/spotify?letter=${letter}&genre=${genre}'
+//After response comes, append track with play button and artist name
+
+//SAVE PLAYLIST button should make a $.ajax POST playlists
+
+//SAVE PLAYLIST button should also change the state so that there are two buttons: one for viewing the playlists collection and one for making a new playlist.
 
 })();
