2d9bc33e7a7e1145c513a3320af6ad812ac55916
Peter Doane <peterdoane@gmail.com>
2016-07-13 19:50:50 -0700

    closes #43 made search buttons dynamic

--- public/js/search.js
+++ public/js/search.js
@@ -44,11 +44,22 @@ const getSpotify = function(index, playlist) {
     var $buttonContainer = $('#both_buttons');
     event.preventDefault();
 
+    var collectionButton = '<a id="collection-button" class="waves-effect grey waves-light btn" href="collections.html">View Collection</a>';
+    var saveButton = '<a id="save-button" class="waves-effect grey waves-light btn">Save Playlist</a>';
+    var makeNewButton = '<a id="newSearch" class="waves-effect grey waves-light btn">Make New</a>';
+
     if ($('#collection-button').length == 0){
-      $buttonContainer.append('<a id="collection-button" class="waves-effect grey waves-light btn" href="collections.html">View Collection</a><a id="save-button" class="waves-effect grey waves-light btn">Save Playlist</a><a id="newSearch" class="waves-effect grey waves-light btn">Make New</a>');
+      $buttonContainer.append(saveButton);
+
+      // create an event handler that appends other 2 buttons on click of saveButton.
     }
     $save = $('#save-button');
-    activateSave();
+    activateSave(function(){
+
+      $buttonContainer.html(collectionButton + makeNewButton);
+
+    });
+
     searchInProgress  = false;
     return;
   }
@@ -126,7 +137,7 @@ $searchInput.keypress(function(event) {
 
 });
 
-var activateSave = function() {
+var activateSave = function(callback) {
   $save.on('click', function(event) {
     var $xhr = $.ajax({
       method: 'POST',
@@ -138,10 +149,12 @@ var activateSave = function() {
 
     $xhr.done(function(response) {
       console.log('success');
+      callback();
     });
 
     $xhr.fail(function(error) {
       console.log(error);
+      // 
     })
   });
 };
